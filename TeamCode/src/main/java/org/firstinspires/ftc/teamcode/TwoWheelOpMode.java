be05719138e9b711544ee3f66b43ff712e2ccc6f
Roushil Satta <roushil.satta@gmail.com>
2026-06-07 15:40:24 -0700

    2wheeldt

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TwoWheelOpMode.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import java.util.List;
+@TeleOp
+public class TwoWheelOpMode extends OpMode {
+    DcMotor left;
+    DcMotor right;
+
+    public void init() {
+        left = hardwareMap.dcMotor.get("left");
+        right = hardwareMap.dcMotor.get("right");
+    }
+
+    public void loop() {
+        right.setPower(gamepad1.right_stick_y);
+        left.setPower(-gamepad1.left_stick_y);
+    }
+}
+
