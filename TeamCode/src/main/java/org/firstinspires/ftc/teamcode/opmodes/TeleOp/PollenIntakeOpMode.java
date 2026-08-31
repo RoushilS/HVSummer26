b1e38bd1d1fedeb94de9568fd20b1129290467c8
Roushil Satta <roushil.satta@gmail.com>
2026-08-30 17:13:51 -0700

    full pollen bot thing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOp/PollenIntakeOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOp/PollenIntakeOpMode.java
@@ -11,26 +11,32 @@ public class PollenIntakeOpMode extends OpMode {
 
     private DcMotorEx intake;
 
-    private Servo lift1;
+    private DcMotorEx slide;
+
+    //private Servo lift1;
     private Servo lift2;
 
     @Override
     public void init() {
         intake = hardwareMap.get(DcMotorEx.class, "intake"); // initialize via hardwareMap
-        lift1 = hardwareMap.get(Servo.class, "lift1"); // initialize via hardwareMap
+        //lift1 = hardwareMap.get(Servo.class, "lift1"); // initialize via hardwareMap
         //lift2 = hardwareMap.get(Servo.class, "lift2"); // initialize via hardwareMap
+        slide = hardwareMap.get(DcMotorEx.class, "slide");
     }
 
     @Override
     public void loop() {
-        double speed = gamepad1.right_trigger-gamepad1.left_trigger; // invert if needed
+        double speed = gamepad2.right_trigger-gamepad2.left_trigger; // invert if needed
         intake.setPower(speed);
-        if (gamepad1.dpad_up) {
+        /*if (gamepad2.dpad_up) {
             lift1.setPosition(0.45);
         }
         else {
             lift1.setPosition(0.608);
-        }
+        }*/
+        slide.setPower(gamepad2.left_stick_y);
+
+
         telemetry.update();
     }
 }
